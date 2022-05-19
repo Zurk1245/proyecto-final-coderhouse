15f75ae9d2b50ed3e7ea8ff5656dfe9034495f2d
Zurk1245 <marianito01@live.com>
2022-05-18 21:13:26 -0300

    Correcion de la creacion de los logs

--- src/winston-logger.js
+++ src/winston-logger.js
@@ -3,8 +3,8 @@ const logger = winston.createLogger({
     level: "silly",
     transports: [
         new winston.transports.Console({ level: "silly" }),
-        new winston.transports.File({ filename: "./logs/warn.log", level: "warn" }),
-        new winston.transports.File({ filename: "./logs/error.log", level: "error" })
+        new winston.transports.File({ filename: "./src/logs/warn.log", level: "warn" }),
+        new winston.transports.File({ filename: "./src/logs/error.log", level: "error" })
     ]
 });
 
